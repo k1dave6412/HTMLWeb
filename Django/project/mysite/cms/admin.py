3b2b4ef46210f8bad9205c150c29defd691a7f03
k1dave6412 <k1dave6412@gmail.com>
2017-06-15 16:08:48 +0800

    web

--- Django/project/mysite/cms/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Restaurant,Food
-
-admin.site.register(Food)
-admin.site.register(Restaurant)
--- Django/project/mysite/myblog/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.utils import timezone
-
-from __future__ import unicode_literals
-# Create your models here.
-
-class Post(models.Model):
-    
